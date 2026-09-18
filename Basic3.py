66178e40ecbbfd49d9425bc0b06b826ae32c4144
khagendra2035 <rawol2035@gmail.com>
2026-09-18 07:49:14 +0545

    basic structure of portfolio

---
+++ Basic3.py
@@ -0,0 +1,77 @@
+#LIST AND TUPLES                  #strings are changable in list where not in python
+# marks=[12,23.4,23,45,67,43]
+# print(marks)
+# print(type(marks))
+# print(marks[0])
+# print(marks[1])
+# print(len(marks))
+
+# student=("ram kumar", 23, "kathmandu")
+# print(student[0])
+# student[0] = "hari"   ##got error#
+# print(student)
+
+# marks=[23,34,45,76,87,34]  #slicing
+# print(marks[1:5])
+# print(marks[-4:-1])
+
+
+##LIST MENTHODS
+# list=[1,2,3]
+# list.append(4)
+# print(list)
+# print(list.sort())
+# print(list)
+# print(list.sort(reverse=True))
+# print(list)
+
+# list=["ram", "shyam", "hari", "lalita"]
+# list.sort()
+# print(list)
+# list.sort(reverse=True)
+# print(list)
+# list.reverse()
+# print(list)
+# list.insert(1,"lakshman")
+# print(list)
+# list.remove("shyam")
+# print(list)
+# list.pop(1)
+# print(list)
+
+#TUPLES IN PYTHON     #List are mutable where tuples are immutable
+# tup=(2,1,3,5,4)
+# print(tup)
+# print(tup[2])
+
+# tup=()
+# print(tup)
+# print(type(tup))   #tuple
+
+# tup=(1)
+# print(tup)
+# print(type(tup))  #integer
+
+# tup=(2.4)
+# print(tup)
+# print(type(tup))  #float
+
+# tup=(2,)
+# print(tup)
+# print(type(tup)) #tuple
+
+# tup=(1,4,2,5,3,6)
+# print(tup[0:5])     #slicing
+##TUPLE METHODS
+# tup=(1,2,3,4,5,6,4)
+# print(tup.index(5))     #tuple indexing it output index not value
+# print(tup.count(4))     #it counts the num of value
+
+
+
+##QUESTIONS#
+#WAP to ask the user to enter names of their 3 favourite movies & store them in a list.append
+#WAP to check if a list contains a palindrome of elements.(Hint:use copy() method)
+
+
+
--- GCD.py
+++
@@ -1,9 +0,0 @@
-#how to print
-# print("hello there!")
-
-#VARIABLES
- name= "khagendra"
-rollno=7
-weigt=52.6
-
-
